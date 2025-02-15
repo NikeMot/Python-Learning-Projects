d42912190f998555938def81110766ee7ebc1ba8
NikeMot <nikennamo@protonmail.com>
2025-02-15 19:59:38 +0000

    Update Day Three Project.py
    Add the fourth requirement to the file

--- Day Three Project.py
+++ Day Three Project.py
@@ -5,6 +5,7 @@ input_letter1 = str(input("Please, select the first letter\n"))
 input_letter2 = str(input("Please, select the second letter\n"))
 input_letter3 = str(input("Please, select the third letter\n"))
 
+
 #Make the text and chosen letters lowercase and check how often the letters appear
 
 lower_text = input_text.lower()
@@ -13,11 +14,13 @@ letter2 = input_letter2.lower()
 letter3 = input_letter3.lower()
 text = tuple(lower_text)
 
+
 #Print how many times each chosen letter appears in the text
 
 print(f"'{letter1}' appears {text.count(letter1)} times in the text")
 print(f"'{letter2}' appears {text.count(letter2)} times in the text")
 print(f"'{letter3}' appears {text.count(letter3)} times in the text")
+
 
 #Calculate and print how many characters and words are in the text
 
@@ -25,7 +28,14 @@ input_words = input_text.split()
 total_words = tuple(input_words)
 print("The total number of characters in the text is", len(text), "and the total number of words is", len(input_words))
 
+
 #Print the first and last characters and words in the text
 
 print("The first and last characters of the text are", text[0], "and", text[-1])
 print("The first and last words of the text are", words[0], "and", words[-1])
+
+
+#Reverse the text, join it and print it
+
+reversed_words = " ".join(words[::-1])
+print(f"Find below the text reversed:\n> {reversed_words}")
