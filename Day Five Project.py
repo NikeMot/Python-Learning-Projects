440363c35e77bc5697358030f65dea5bf8dbb95f
NikeMot <nikennamo@protonmail.com>
2025-03-06 20:32:12 +0000

    Update Day Five Project.py
    Add the function that outputs the dash line corresponding to the number of letters

--- Day Five Project.py
+++ Day Five Project.py
@@ -6,3 +6,12 @@ def rules():
   secret_word = choice(word_list)
   selected_letter = input("Welcome to HANGMAN! You have 6 tries to guess the word I'm thinking".lower())
   return selected_letter, secret_word
+
+def dash_output(selected_letter,secret_word):
+  dash_list = []
+  letter_list = []
+  for letter in secret_word:
+    letter_list.append(letter)
+    dash_list.append(" _ ")
+  print("Guess the word", dash_list)
+  return letter_list,dash_list,secret_word,selected_letter
