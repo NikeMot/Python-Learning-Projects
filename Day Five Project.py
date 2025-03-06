c7ceb54abfc2a4b9c1cedfe03c1a471e08bad28b
NikeMot <nikennamo@protonmail.com>
2025-03-06 21:41:59 +0000

    Update Day Five Project.py
    Add the main game to the project

--- Day Five Project.py
+++ Day Five Project.py
@@ -15,3 +15,40 @@ def dash_output(selected_letter,secret_word):
     dash_list.append(" _ ")
   print("Guess the word", dash_list)
   return letter_list,dash_list,secret_word,selected_letter
+
+def main_game(letter_list,dash_list,selected_letter,secret_word):
+  
+  count = 0
+  
+  
+  body_dict {1:"Incorrect, right leg gone", 
+             2:"Nope, left leg gone", 
+             3:"Wrong, body gone", 
+             4:"Negative, right arm gone", 
+             5:"Not at all, left arm gone", 
+             6:"YOU LOST!! Head's gone :( "}
+  
+  while count < 6:
+    
+    if letter_list.find(selected_letter) == -1:
+      
+      count += 1
+      print(body_dict[count])
+      print("")
+      
+      
+    else:
+      for index in enumerate(letter_list):
+        if selected_letter == letter_list[index]
+        dash_list.pop(index)
+        dash_list.insert(index, selected_letter)
+        print("You guessed correctly! " , dash_list)
+        if dash_list == letter_list:
+          print("You won! Well done!")
+          return 
+      
+      
+
+  else:
+    print(body_dict[6])
+    return 
