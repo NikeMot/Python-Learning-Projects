59b9a7fb9a96e0f10fafdbdbc150fb914e1d049f
NikeMot <nikennamo@protonmail.com>
2025-03-07 14:31:52 +0000

    Update Day Five Project.py
    First working version

--- Day Five Project.py
+++ Day Five Project.py
@@ -11,23 +11,26 @@ def letter_select(word_list):
     selected_letter = input("Make sure to select only 1 letter at a time\n").lower()
     if len(selected_letter) == 1 and selected_letter.isalpha():
       return selected_letter
-      
-def dash_output(secret_word,selected_letter):
-  
+
+def dash_output(secret_word): 
+  dash_list = []
+  letter_list = []
+
   for letter in secret_word:
     letter_list.append(letter)
-    dash_list.append(" _ ")
-  print("Guess the word", dash_list)
-  return letter_list,dash_list,secret_word,selected_letter
+    dash_list.append("_")
+  on_screen_dash = "  ".join(dash_list)
+  
+  print("Guess the word", on_screen_dash)
+  
+  return letter_list,dash_list,secret_word,on_screen_dash
 
 def main_game(word_list):
-
-  count = 0
   
   secret_word = choice(word_list)
   
-  letter_list,dash_list,secret_word = dash_output(secret_word)
-
+  count = 0
+ 
   body_dict = {1:"Incorrect, right leg gone", 
              2:"Nope, left leg gone", 
              3:"Wrong, body gone", 
@@ -36,26 +39,33 @@ def main_game(word_list):
              6:"YOU LOST!! Head's gone :( "}
   
   print("Welcome to Hangman! You have 6 tries to get the secret word")
+
+   
+  letter_list,dash_list,secret_word,on_screen_dash = dash_output(secret_word)
   
   while count < 6:
-    letter_list,dash_list,secret_word,selected_letter = dash_output(secret_word,selected_letter)
+    selected_letter=letter_select(word_list)
+    print(on_screen_dash)
     
-    if letter_list.find(selected_letter) == -1:
-      
+    if str(letter_list).find(selected_letter) == -1:
       count += 1
       print(body_dict[count])
-      print("")
-      
-      
+      print("\n")
+      continue
+        
     else:
-      for index in enumerate(letter_list):
+      for index,number in enumerate(letter_list):
         if selected_letter == letter_list[index]:
           dash_list.pop(index)
           dash_list.insert(index, selected_letter)
-          print("You guessed correctly! " , dash_list)
+          on_screen_dash = "  ".join(dash_list)
+          print("You guessed correctly! " , on_screen_dash)
+          print("\n")
+                    
           if dash_list == letter_list:
             print("You won! Well done!")
-            return 
+            print("\n")
+            return
       
       
 
@@ -63,4 +73,5 @@ def main_game(word_list):
     print(body_dict[6])
     return
 
-main_game()
+
+main_game(word_list)
