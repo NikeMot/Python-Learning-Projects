3109f9d41feda0e82cd05b7e2e5ef0ca30f7d61f
NikeMot <nikennamo@protonmail.com>
2025-03-10 15:32:17 +0000

    Create Day Six Project.py

---
+++ Day Six Project.py
@@ -0,0 +1,19 @@
+from pathlib import Path
+
+def display_categories():
+    pass
+
+def modify_categories():
+    pass
+
+def display_recipes():
+    pass
+
+def modify_recipes():
+    pass
+
+
+def main():
+
+    print("""Welcome to the Nikenna Electronic Recipe book!\n
+    The recipe is currently stored in""", Path.home())
