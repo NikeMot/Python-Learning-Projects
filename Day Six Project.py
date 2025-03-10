963979c92931ec85d653fe9242100bb5cd238a67
NikeMot <nikennamo@protonmail.com>
2025-03-10 17:01:40 +0000

    Update Day Six Project.py
    Make changes to import_recipe_book and main fucntion

--- Day Six Project.py
+++ Day Six Project.py
@@ -2,16 +2,17 @@ from pathlib import Path
 import os
 
 
+#Import the recipe book from a specific location
 def import_recipe_book():
-    #Import the recipe book from a specific location
     while True:
-        book_path = Path(input("Please, input the correct path of the recipe book\n"))
+        book_path = Path(input("Please, input the path of the recipe book\n"))
+        print("\n")
         try:
             os.chdir(book_path)
             return book_path
 
         except FileNotFoundError:
-            import_recipe_book()
+            print("Could not find the directory. Please try again")
 
 def display_categories():
     pass
@@ -26,7 +27,11 @@ def modify_recipes():
     pass
 
 
-def main():
+def main(book_path):
 
-    print("""Welcome to the Nikenna Electronic Recipe book!\n
-    The recipe is currently stored in""", Path.home())
+    print(f"Welcome to the Nikenna Electronic Recipe book!\nThe recipe is currently stored in {book_path} \n")
+    return
+
+book_path = import_recipe_book()
+main(book_path)
+
