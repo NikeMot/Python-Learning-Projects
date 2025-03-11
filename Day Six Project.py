283d09526c0307d7c4331d186b7b4fff759b89e5
NikeMot <nikennamo@protonmail.com>
2025-03-11 14:28:50 +0000

    Update Day Six Project.py
    Add display_categories and modify_categories to the program

--- Day Six Project.py
+++ Day Six Project.py
@@ -14,11 +14,34 @@ def import_recipe_book():
         except FileNotFoundError:
             print("Could not find the directory. Please try again")
 
-def display_categories():
-    pass
+def display_categories(book_path):
+    #Allows to display all the categories of the recipe book
+    categories = [folder.name for folder in book_path.glob("*/")]
+    print(f"The available categories are: {categories}")
+    return categories
 
-def modify_categories():
-    pass
+def modify_categories(book_path,categories):
+    #Allows to delete or add categories to the recipe book
+    while True:
+        action = (input("""Would you like to add (a) or remove (r) a category?\n
+        You can also exit by clicking (e)""")).lower()
+        if action == a:
+            new_category = str(input("What would you like to call the new category")).capitalize()
+            try:
+                os.mkdir(Path(book_path, new_category))
+                print(f"{new_category} has been added!")
+            except OSError:
+                print("This folder exists already!")
+        elif action == r:
+            removed_category = str(input("What would you like to remove category")).capitalize()
+            try:
+                os.rmdir(Path(book_path, removed_category))
+            except OSError:
+                print("Please verify the content of the folder and make sure it exist and it is empty")
+        elif action == e:
+            break
+        else:
+            print("Please make sure to select a,r or e ")
 
 def display_recipes():
     pass
